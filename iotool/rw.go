6eb0acad78e51ca04ca5f4736c1702b2e024e884
xxxsen <naivesen@gmail.com>
2024-04-24 23:11:41 +0800

    chore: rwc

--- iotool/rw.go
+++ iotool/rw.go
@@ -2,22 +2,35 @@ package iotool
 
 import "io"
 
-type wrapReadWriter struct {
+type ioWrapper struct {
 	r io.Reader
 	w io.Writer
+	c io.Closer
 }
 
-func (w *wrapReadWriter) Read(b []byte) (int, error) {
+func (w *ioWrapper) Read(b []byte) (int, error) {
 	return w.r.Read(b)
 }
 
-func (w *wrapReadWriter) Write(b []byte) (int, error) {
+func (w *ioWrapper) Write(b []byte) (int, error) {
 	return w.w.Write(b)
 }
 
+func (w *ioWrapper) Close() error {
+	return w.c.Close()
+}
+
 func WrapReadWriter(r io.Reader, w io.Writer) io.ReadWriter {
-	return &wrapReadWriter{
+	return &ioWrapper{
 		r: r,
 		w: w,
+	}
+}
+
+func WrapReadWriteCloser(r io.Reader, w io.Writer, c io.Closer) io.ReadWriteCloser {
+	return &ioWrapper{
+		r: r,
+		w: w,
+		c: c,
 	}
 }
