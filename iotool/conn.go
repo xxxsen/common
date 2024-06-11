dea565a2e141fd62150dfc641ee9f6736d2f4c0f
xxxsen <naivesen@gmail.com>
2024-06-11 22:16:06 +0800

    chore: add ioconn

--- iotool/conn.go
+++ iotool/conn.go
@@ -5,39 +5,105 @@ import (
 	"net"
 )
 
-type wrapConn struct {
+type IOConn struct {
+	r     io.Reader
+	w     io.Writer
+	c     io.Closer
+	lAddr net.Addr
+	rAddr net.Addr
 	net.Conn
-	r io.Reader
-	w io.Writer
-	c io.Closer
+}
+
+type netAddr struct {
+	network string
+	addr    string
+}
+
+func (na netAddr) Network() string {
+	return na.network
+}
+
+func (na netAddr) String() string {
+	return na.addr
+}
+
+func newNetAddrWithString(network string, addr string) net.Addr {
+	return netAddr{network: network, addr: addr}
 }
 
 func WrapConn(basic net.Conn, r io.Reader, w io.Writer, c io.Closer) net.Conn {
-	if r == nil {
-		r = basic
+	ioc := NewIOConn(basic)
+	if r != nil {
+		ioc.ReplaceReader(r)
 	}
-	if w == nil {
-		w = basic
+	if w != nil {
+		ioc.ReplaceWriter(w)
 	}
-	if c == nil {
-		c = basic
+	if c != nil {
+		ioc.ReplaceCloser(c)
 	}
-	return &wrapConn{
-		Conn: basic,
-		r:    r,
-		w:    w,
-		c:    c,
+	return ioc
+}
+
+func NewIOConn(base net.Conn) *IOConn {
+	return &IOConn{
+		r:     base,
+		w:     base,
+		c:     base,
+		lAddr: base.LocalAddr(),
+		rAddr: base.RemoteAddr(),
 	}
 }
 
-func (w *wrapConn) Read(b []byte) (int, error) {
-	return w.r.Read(b)
+func (c *IOConn) ReplaceReader(r io.Reader) *IOConn {
+	c.r = r
+	return c
 }
 
-func (w *wrapConn) Write(b []byte) (int, error) {
-	return w.w.Write(b)
+func (c *IOConn) ReplaceWriter(w io.Writer) *IOConn {
+	c.w = w
+	return c
 }
 
-func (w *wrapConn) Close() error {
-	return w.c.Close()
+func (c *IOConn) ReplaceCloser(cr io.Closer) *IOConn {
+	c.c = cr
+	return c
+}
+
+func (c *IOConn) ReplaceLocalAddr(addr net.Addr) *IOConn {
+	c.lAddr = addr
+	return c
+}
+
+func (c *IOConn) ReplaceRemoteAddr(addr net.Addr) *IOConn {
+	c.rAddr = addr
+	return c
+}
+
+func (c *IOConn) ReplaceLocalAddrWithString(network string, addr string) *IOConn {
+	return c.ReplaceLocalAddr(newNetAddrWithString(network, addr))
+}
+
+func (c *IOConn) ReplaceRemoteAddrWithString(network string, addr string) *IOConn {
+	return c.ReplaceRemoteAddr(newNetAddrWithString(network, addr))
+}
+
+func (c *IOConn) Read(b []byte) (n int, err error) {
+	return c.r.Read(b)
+}
+
+func (c *IOConn) Write(b []byte) (n int, err error) {
+	return c.w.Write(b)
+}
+
+func (c *IOConn) Close() error {
+	return c.c.Close()
+}
+
+func (c *IOConn) LocalAddr() net.Addr {
+	return c.lAddr
+}
+
+func (c *IOConn) RemoteAddr() net.Addr {
+	return c.rAddr
 }
