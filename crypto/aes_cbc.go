c31cff24e704f0ea006ea84f39d8a19647cd0f72
xxxsen <naivesen@gmail.com>
2024-06-22 15:11:12 +0800

    chore: util func

--- crypto/aes_cbc.go
+++ crypto/aes_cbc.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -12,22 +11,13 @@ import (
 type aesCBC struct {
 }
 
-func (c *aesCBC) pad(src []byte) []byte {
-	padding := aes.BlockSize - len(src)%aes.BlockSize
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	out := make([]byte, len(padText)+len(src))
-	copy(out, src)
-	copy(out[len(src):], padText)
-	return out
-}
-
 func (c *aesCBC) Encrypt(plaintext []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	plaintext = c.pad(plaintext)
+	plaintext = Padding(plaintext, aes.BlockSize)
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
@@ -38,18 +28,6 @@ func (c *aesCBC) Encrypt(plaintext []byte, key []byte) ([]byte, error) {
 	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
 
 	return ciphertext, nil
-}
-
-func (c *aesCBC) unpad(src []byte) ([]byte, error) {
-	length := len(src)
-	if length == 0 {
-		return nil, fmt.Errorf("unpad: input data is empty")
-	}
-	padding := int(src[length-1])
-	if padding > length || padding > aes.BlockSize {
-		return nil, fmt.Errorf("unpad: invalid padding size")
-	}
-	return src[:length-padding], nil
 }
 
 func (c *aesCBC) Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
@@ -72,7 +50,7 @@ func (c *aesCBC) Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	mode := cipher.NewCBCDecrypter(block, iv)
 	mode.CryptBlocks(ciphertext, ciphertext)
 
-	plaintext, err := c.unpad(ciphertext)
+	plaintext, err := UnPadding(ciphertext, aes.BlockSize)
 	if err != nil {
 		return nil, err
 	}
--- crypto/utils.go
+++ crypto/utils.go
@@ -1,8 +1,33 @@
 package crypto
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
 func DeriveKey(in []byte) []byte {
 	hash := sha256.Sum256(in)
 	return hash[:]
+}
+
+func Padding(src []byte, blksize int) []byte {
+	padding := blksize - len(src)%blksize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	out := make([]byte, len(padText)+len(src))
+	copy(out, src)
+	copy(out[len(src):], padText)
+	return out
+}
+
+func UnPadding(src []byte, blksize int) ([]byte, error) {
+	length := len(src)
+	if length == 0 {
+		return nil, fmt.Errorf("unpad: input data is empty")
+	}
+	padding := int(src[length-1])
+	if padding > length || padding > blksize {
+		return nil, fmt.Errorf("unpad: invalid padding size")
+	}
+	return src[:length-padding], nil
 }
