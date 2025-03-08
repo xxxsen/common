4bcaea4331a1b790562abc8334f4b2438ff69ffe
xxxsen <naivesen@gmail.com>
2025-03-08 20:15:15 +0800

    chore: remove unused code

--- database/config.go
+++ database/config.go
@@ -1,7 +1,6 @@
 package database
 
 type DBConfig struct {
-	Kind string `json:"kind"`
 	Host string `json:"host"`
 	Port uint32 `json:"port"`
 	User string `json:"user"`
--- database/database.go
+++ database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"database/sql"
-	"fmt"
 )
 
 type IQueryer interface {
@@ -17,19 +16,4 @@ type IExecer interface {
 type IQueryExecer interface {
 	IQueryer
 	IExecer
-}
-
-func buildSqlDataSource(c *DBConfig) string {
-	return fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8mb4", c.User, c.Pwd, "tcp", c.Host, c.Port, c.DB)
-}
-
-func InitDatabase(c *DBConfig) (*sql.DB, error) {
-	client, err := sql.Open("mysql", buildSqlDataSource(c))
-	if err != nil {
-		return nil, fmt.Errorf("open db failed, err:%w", err)
-	}
-	if err := client.Ping(); err != nil {
-		return nil, fmt.Errorf("ping failed, err:%w", err)
-	}
-	return client, nil
 }
