79cd85fe6ac87ae552f125c0e86c5be400a4bbfb
xxxsen <naivesen@gmail.com>
2025-10-12 01:39:36 +0800

    chore: yaml support

--- logger/config.go
+++ logger/config.go
@@ -1,10 +1,10 @@
 package logger
 
 type LogConfig struct {
-	File      string `json:"file"`
-	Level     string `json:"level"`
-	FileSize  uint64 `json:"file_size"`
-	FileCount uint64 `json:"file_count"`
-	KeepDays  uint32 `json:"keep_days"`
-	Console   bool   `json:"console"`
+	File      string `json:"file" yaml:"file"`
+	Level     string `json:"level" yaml:"level"`
+	FileSize  uint64 `json:"file_size" yaml:"file_size"`
+	FileCount uint64 `json:"file_count" yaml:"file_count"`
+	KeepDays  uint32 `json:"keep_days" yaml:"keep_days"`
+	Console   bool   `json:"console" yaml:"console"`
 }
